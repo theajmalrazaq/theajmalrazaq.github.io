f3ea83bbaa2d9f7eaa70e4df1bbf23ac5bd19b2c
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-07-13 09:17:08 -0700

    added:react,supabase

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://theajmalrazaq.github.io/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "static",
+  integrations: [react()],
 });
