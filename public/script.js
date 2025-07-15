6fa4d28c8bb699e42c4a9b4e7dec1dc1bf09d036
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-07-15 00:11:08 -0700

    added:laoding

--- public/script.js
+++ public/script.js
@@ -1,16 +1,6 @@
 import Lenis from "https://cdn.jsdelivr.net/npm/@studio-freight/lenis@1.0.39/dist/lenis.mjs";
 import AOS from "https://cdn.jsdelivr.net/npm/aos@2.3.4/+esm";
 
-document.addEventListener("DOMContentLoaded", () => {
-  AOS.init({
-    duration: 800,
-    easing: "ease",
-    once: false,
-    mirror: false,
-    anchorPlacement: "top-bottom",
-  });
-});
-
 const lenis = new Lenis({
   lerp: 0.08,
   wheelMultiplier: 1.2,
@@ -21,3 +11,20 @@ function raf(time) {
   requestAnimationFrame(raf);
 }
 requestAnimationFrame(raf);
+document.addEventListener("DOMContentLoaded", function () {
+  const loadingOverlay = document.getElementById("loading-overlay");
+  document.body.style.overflow = "hidden";
+  if (loadingOverlay) {
+    setTimeout(() => {
+      loadingOverlay.style.display = "none";
+      document.body.style.overflow = "auto";
+      AOS.init({
+        duration: 800,
+        easing: "ease",
+        once: false,
+        mirror: false,
+        anchorPlacement: "top-bottom",
+      });
+    }, 1000);
+  }
+});
