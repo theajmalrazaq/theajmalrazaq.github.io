e290a6e5eae09f61863afc6f8d3444ef1499de4f
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-07-15 11:43:08 -0700

    added:pojectscard

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,17 +23,21 @@ export default function Projects({ children }) {
 
   return (
     <section>
-      <div className="flex flex-col items-center justify-center gap-8 p-6 w-full">
-        {projects.map((project) => (
+      <div
+        className="flex flex-col items-center justify-center gap-8 p-6 w-full"
+        data-aos="fade-up"
+      >
+        {projects.map((project, index) => (
           <div
             key={project.id}
-            className="project-card text-white bg-black border border-white/10 p-4 rounded-3xl shadow-lg flex sm:flex-row flex-col shadow-accent items-start gap-4 w-auto "
+            className="project-card text-white bg-black border border-white/10 p-4 rounded-3xl shadow-lg flex sm:flex-row flex-col shadow-accent items-start gap-4 sm:w-[90%] w-full"
+            data-aos="zoom-in"
           >
             <div>
               <img
                 src={project.img}
                 alt={project.name}
-                className="w-full h-48 object-cover rounded-lg mb-4"
+                className="w-full h-56"
               />
             </div>
             <div>
@@ -47,15 +51,14 @@ export default function Projects({ children }) {
 
               <div className="flex justify-between items-center h-full">
                 <div className="relative mt-4 flex items-center justify-between w-fit ">
-                  <div className=" bg-accent w-5 h-9 absolute rounded-full"></div>
                   <a
                     href={project.url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="z-99 relative"
+                    className="relative z-10"
                   >
                     <div
-                      className="w-fit bg-black/5 border text-accent sm:px-3 px-1 py-2 border-white/10 rounded-xl backdrop-blur-2xl hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer sm:text-base text-sm flex justify-center items-center gap-2"
+                      className="w-fit bg-gradient-to-r from-accent/5 to-bg-black/5 border text-accent sm:px-3 px-3 py-2 border-white/10 rounded-full backdrop-blur-2xl hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer sm:text-base text-sm flex justify-center items-center gap-2"
                       aria-label="View Project"
                     >
                       View Project
@@ -65,14 +68,12 @@ export default function Projects({ children }) {
                     </div>
                   </a>
                 </div>
-
                 <a
                   href={project.github}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  {" "}
-                  Github{" "}
+                  Github
                 </a>
               </div>
             </div>
